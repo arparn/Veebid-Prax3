b0d6c7b2670522a792eaa3881b3ae858cf63d847
ARTHUR-ALEKSANDER PÄRNOJA <arthurp200148@gmail.com>
2020-11-11 22:53:59 +0200

    Getting started

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
 <?php
-    echo "Hello World!";
+
+$title = "My first php web page!";
+$content = "Hello World!";
+
+/*
+ * $_GET
+ * $_POST
+ * $_FILES
+ *
+ * $_SESSION
+ * $_SERVER
+ */
+
+$num = 1 + strlen($title);
+$num2 = $_GET["num"] ?? 0; # Берем переменную $num из поисковой строки. Try: localhost/prax3/?num=10
+$num3 = isset($_GET["num"]) ? $_GET["num"] : 0 # $num2 = $num3
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Index</title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+
+        <h1><?php echo $title;?></h1>
+        <p><?php echo $content;?></p>
+
+        <h2><?php echo $num;?></h2>
+
+    </body>
+
+</html>
